967dd2dfa2aaf05f59b7d4044970e230cd8f19e1
Brom Bresenham <brom.bresenham@runegate.com>
2024-01-21 12:28:52 -0500

    [MoltenVK] Starting on SDL+Vulkan/MoltenVK framework

---
+++ Platforms/macOS/Warlock/Test.m
@@ -0,0 +1,8 @@
+//
+//  Test.m
+//  Warlock
+//
+//  Created by Abe Pralle on 1/21/24.
+//
+
+#import <Foundation/Foundation.h>
---
+++ Platforms/macOS/Warlock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Warlock
+//
+//  Created by Abe Pralle on 1/21/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
